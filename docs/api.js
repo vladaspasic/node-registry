14e7747900b4ec56e76db331771e7cc14a6cb10d
Vladimir Spasic <vspasic@admins-MacBook-Pro-2.local>
2014-12-08 01:22:44 +0100

    Add yui docs

---
+++ docs/api.js
@@ -0,0 +1,25 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Container",
+        "DAG",
+        "EventEmitter",
+        "Holder",
+        "Module",
+        "Object",
+        "OrederedConfiguration",
+        "Registry",
+        "Server"
+    ],
+    "modules": [
+        "Registry"
+    ],
+    "allModules": [
+        {
+            "displayName": "Registry",
+            "name": "Registry",
+            "description": "Node Registry Module"
+        }
+    ]
+} };
+});
